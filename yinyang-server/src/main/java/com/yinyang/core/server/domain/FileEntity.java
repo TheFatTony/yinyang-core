9e469cfb70516748e5a008c4d3b81e95e274d799
toolstik <tolstik.va@gmail.com>
2019-02-14 17:29:32 +0300

    file-user relationship added

--- yinyang-server/src/main/java/com/yinyang/core/server/domain/FileEntity.java
+++ yinyang-server/src/main/java/com/yinyang/core/server/domain/FileEntity.java
@@ -37,5 +37,10 @@ public class FileEntity extends YAbstractPersistable<Long> {
     @Column(name = "file_size")
     private Long fileSize;
 
+    @Getter
+    @Setter
+    @Column(name = "user_id")
+    private UserEntity user;
+
 
 }
